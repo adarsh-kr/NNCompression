d96c764208ebaa667609225629dd8799610e9ef7
AdarshKr <adi28kr@gmail.com>
2019-02-13 15:42:28 -0600

    ..

--- Compression/layerDump/TestData/test.py
+++ Compression/layerDump/TestData/test.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 
-labels = np.genfromtxt('labels-1_-1')
-a = [2, 2, 19, 2]
+labels = np.genfromtxt('batch_32/labels-1_-1')
+a = [3, 3, 23, 3]
 
 for i in range(4):
     for j in range(a[i]):
-        c = np.genfromtxt('labels{0}_{1}'.format(i+1, j+1))
+        #print(i+1, j)
+        c = np.genfromtxt('batch_64/labels{0}_{1}'.format(i+1, j))
         print((c==labels).sum())
+        #        print()
+        #print("labels{0}_{1}_{2}".format(i+1, j, (c==labels).sum()))
         
 
 
